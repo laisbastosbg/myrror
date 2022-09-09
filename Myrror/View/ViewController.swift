3b14bc659435d0dae2b5dfc99a2d96e4c9eb5c22
Lais Godinho <lais2bg@gmail.com>
2022-09-09 11:08:25 -0300

    :construction: feat: estrutura da pagina inicial vazia

--- Myrror/View/ViewController.swift
+++ Myrror/View/ViewController.swift
@@ -8,35 +8,103 @@
 import UIKit
 
 class ViewController: UIViewController {
+    lazy var pageTitle: UILabel = {
+        let label = UILabel()
+        let dayFormatter = DateFormatter()
+        dayFormatter.dateFormat = "dd"
+        let day = dayFormatter.string(from: Date.now)
+        
+        let monthFormatter = DateFormatter()
+        monthFormatter.dateFormat = "MMMM"
+        let month = monthFormatter.string(from: Date.now)
+        
+        let today = "\(day) de \(month)"
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.text = today
+        label.textColor = UIColor(named: "TextColor")
+        
+        return label
+    }()
+    
+    lazy var calendarButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.setImage(UIImage(systemName: "calendar"), for: .normal)
+        return button
+    }()
+    
+    lazy var emptyScreenImage: UIImageView = {
+        let image = UIImageView()
+        
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "empty_home")
+        
+        return image
+    }()
+    
+    lazy var emptyScreenLabel: UILabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Nada aqui ainda 😕"
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = UIColor(named: "TextColor")
+        
+        return label
+    }()
+    
     lazy var navigationButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("proxima tela", for: .normal)
-        button.backgroundColor = .tintColor
-        button.setTitleColor(UIColor(named: "TextColor"), for: .normal)
+        button.setTitle("Nova Reflection", for: .normal)
+        button.configuration = .filled()
+        button.setTitleColor(.white, for: .normal)
         button.addTarget(self, action: #selector(self.navigate), for: .touchUpInside)
-        button.layer.shadowColor = CGColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1)
-        button.layer.shadowOpacity = 1
-        button.layer.cornerRadius = 5
-        button.layer.shadowOffset = CGSize(width: 1, height: 5)
         button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
         
         return button
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(navigationButton)
+        view.addSubview(pageTitle)
+        view.addSubview(calendarButton)
+        view.addSubview(emptyScreenImage)
+        view.addSubview(emptyScreenLabel)
         
         view.backgroundColor = UIColor(named: "Primary")
         
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
         NSLayoutConstraint.activate([
-            navigationButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            navigationButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            navigationButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
-            navigationButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
-            navigationButton.heightAnchor.constraint(equalToConstant: 50)
+            
+            pageTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            pageTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            
+            calendarButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            calendarButton.centerYAnchor.constraint(equalTo: pageTitle.centerYAnchor),
+            
+            emptyScreenImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emptyScreenImage.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -32),
+            emptyScreenImage.heightAnchor.constraint(equalToConstant: 312),
+            
+            emptyScreenLabel.topAnchor.constraint(equalTo: emptyScreenImage.bottomAnchor, constant: 32),
+            emptyScreenLabel.centerXAnchor.constraint(equalTo: emptyScreenImage.centerXAnchor),
+            
+            
+//            navigationButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            navigationButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            navigationButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            navigationButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
+            navigationButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -48),
+            navigationButton.heightAnchor.constraint(equalToConstant: 48),
         ])
     }
     
