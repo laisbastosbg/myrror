8ca385ea9ce778c520beaa14829125def1dc39c7
ChristianPaulo <christianpaulodev@gmail.com>
2022-09-14 12:09:32 -0300

    :sparkles: feat: add lib lottie files

---
+++ Myrror/View/EmojiViewController.swift
@@ -0,0 +1,40 @@
+//
+//  EmojiViewController.swift
+//  Myrror
+//
+//  Created by Christian Paulo on 14/09/22.
+//
+
+import UIKit
+import Lottie
+class EmojiViewController: UIViewController {
+
+    let animationView = AnimationView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAnimation()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setupAnimation() {
+        animationView.animation = Animation.named("puto")
+        animationView.frame = view.bounds
+        animationView.backgroundColor = .white
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        view.addSubview(animationView)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
